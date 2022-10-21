8dec658beb8d641be51e1319b12e91d42280d1a8
Anthony Chu <anthony@anthonychu.ca>
2022-10-20 23:48:16 -0700

    Add start script and port

--- app.js
+++ app.js
@@ -96,7 +96,7 @@ async function rerunLatestWorkflow({githubToken, owner, repo}) {
 
 const start = async () => {
   try {
-    await fastify.listen({ port: 3000 })
+    await fastify.listen({ port: process.env.PORT || 3000, host: '0.0.0.0' })
   } catch (err) {
     fastify.log.error(err)
     process.exit(1)
